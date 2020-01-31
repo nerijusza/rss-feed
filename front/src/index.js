91fd21459a0a4cbd0e7ebb1ff473dfa9be0fd0b9
Nerijus Zaniauskas <nerijus.zaniauskas@gmail.com>
2020-01-31 14:30:01 +0200

    Create authorised content component
    Load frequent words and rss feed from API. Styling will be done later.

--- front/src/index.js
+++ front/src/index.js
@@ -4,8 +4,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
 import Alert from 'react-bootstrap/Alert';
+import Container from 'react-bootstrap/Container';
 
-class MyComponent extends React.Component {
+class FrequentWords extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -48,4 +49,70 @@ class MyComponent extends React.Component {
     }
 }
 
-ReactDOM.render(<MyComponent />, document.getElementById('root'));
+
+class RssFeed extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            items: []
+        };
+    }
+
+    componentDidMount() {
+        fetch("http://127.0.0.1:8080/api/rss")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        items: result.feed.entry
+                    });
+                },
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+    }
+
+    render() {
+        const { error, isLoaded, items } = this.state;
+        if (error) {
+            return <div>Error: {error.message}</div>;
+        } else if (!isLoaded) {
+            return <Alert variant="primary">Loading RSS feed items</Alert>;
+        } else {
+            return (
+                <Container>{items.map(entry => <RssEntry entry={entry} />)}</Container>
+            );
+        }
+    }
+}
+
+function AuthorizedContent() {
+    return (
+        <Container>
+            <FrequentWords />
+            <RssFeed />
+        </Container>
+    );
+}
+
+function RssEntry(props) {
+    return (
+        <div>
+            Updated: {props.entry.updated}<br />
+            Author name: {props.entry.author.name}<br />
+            Author url: {props.entry.author.uri}<br />
+            URL: {props.entry.link.href}<br />
+            Title: {props.entry.title}<br />
+            Summary: {props.entry.summary}<br />
+        </div>
+    );
+}
+
+ReactDOM.render(<AuthorizedContent />, document.getElementById('root'));
